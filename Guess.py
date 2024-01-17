b1cd77daccc485e4d34e39f76c1dc1768377e319
edinabwari <bwarikwambokaedina@gmail.com>
2024-01-17 14:40:11 +0300

    Final commit

--- Guess.py
+++ Guess.py
@@ -1,20 +1,29 @@
 import random 
+random_number = random.randint(1,20)
+guess_count = 0
+
 def is_numeric(Value):
     try:
         float(Value)
         return True
     except ValueError:
         return False
-random_number = random.randint(1,20)
-#print("Random number is : ", random_number)
-user_number = input("Enter a number from 1 to 20: ")
-if int(user_number) == random_number:
-    print(f"user number is {user_number} while random number is {random_number}")
-
-elif int(user_number) < random_number:
-    less = random_number - int(user_number)
-    print(f"user number is {user_number} while random number is{random_number} thus less {less}")
     
-else:
-    more = int(user_number) - random_number
-    print(f"user number is {user_number} while random number is {random_number} thus more {more}")
+while True:
+    user_number = input("Enter a number from 1 to 20 : ")
+    print()
+    
+    if is_numeric(user_number):
+        user_number = float(user_number)
+        guess_count += 1
+        
+        
+        if float(user_number) < random_number:
+            print("The user number is less than the random number ")
+        elif user_number > random_number:
+            print("The user number is greater than the random number")
+        else:
+            print(f"Congratulations! You guessed the correct number {random_number} in {guess_count} attempts.")
+            break
+    else:
+        print("Invalid input! Enter a valid number.")
